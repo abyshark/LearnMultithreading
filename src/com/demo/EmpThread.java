1424216fc3b689702ff75f2b85b1c6f51f723aba
Abhishek <abhishekst.2021@gmail.com>
2023-12-09 01:29:52 +0530

    Employee Thread class created

---
+++ src/com/demo/EmpThread.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.demo;
+
+/**
+ * @author abhis
+ *
+ */
+public class EmpThread implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
